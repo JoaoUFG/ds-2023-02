9205f87bfa7d7b391c2de08b20090c91c4e66bf4
JoaoUFG <joao.tavares@discente.ufg.br>
2023-10-30 21:34:34 -0300

    Finalizando a aitividade 30/10 (2/2)

---
+++ decorator/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import model.Constante;
+import model.Multiplica;
+import model.Soma;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(new Soma(
+                new Constante(5F),
+                new Multiplica(new Constante(2F),new Constante(3F))
+        ).valor());
+
+    }
+}
---
+++ decorator/src/main/java/model/Expressao.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Expressao {
+    float valor();
+}
